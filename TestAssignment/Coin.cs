bef3e7cd59c1022748f6ded018cb5257dd9c3915
VasylMartynWork <killermbjustthink@gmail.com>
2023-05-06 17:11:15 +0300

    First commit, added output top 10 currencies and added GUI

---
+++ TestAssignment/Coin.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAssignment
+{
+    public class Coin : INotifyPropertyChanged
+    {
+        public string id { get; set; }
+        public string rank { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
+        public string supply { get; set; }
+        public string maxSupply { get; set; }
+        public string marketCapUsd { get; set; }
+        public string volumeUsd24Hr { get; set; }
+        public string priceUsd { get; set; }
+        public string changePercent24H { get; set; }
+
+        //public string Id
+        //{
+        //    get { return id; }
+        //    set
+        //    {
+        //        id = value;
+        //        OnPropertyChanged("Id");
+        //    }
+        //}
+        //public string Symbol
+        //{
+        //    get { return Symbol; }
+        //    set
+        //    {
+        //        symbol = value;
+        //        OnPropertyChanged("Symbol");
+        //    }
+        //}
+        //public string Name
+        //{
+        //    get { return name; }
+        //    set
+        //    {
+        //        name = value;
+        //        OnPropertyChanged("Name");
+        //    }
+        //}
+        //public string Supply
+        //{
+        //    get { return supply; }
+        //    set
+        //    {
+        //        supply = value;
+        //        OnPropertyChanged("Supply");
+        //    }
+        //}
+        //public string MaxSupply
+        //{
+        //    get { return maxSupply; }
+        //    set
+        //    {
+        //        maxSupply = value;
+        //        OnPropertyChanged("MaxSupply");
+        //    }
+        //}
+        //public string MarketCapUsd
+        //{
+        //    get { return marketCapUsd; }
+        //    set
+        //    {
+        //        marketCapUsd = value;
+        //        OnPropertyChanged("MarketCapUsd");
+        //    }
+        //}
+        //public string VolumeUsd24Hr
+        //{
+        //    get { return volumeUsd24Hr; }
+        //    set
+        //    {
+        //        volumeUsd24Hr = value;
+        //        OnPropertyChanged("VolumeUsd24Hr");
+        //    }
+        //}
+        //public string PriceUsd
+        //{
+        //    get { return priceUsd; }
+        //    set
+        //    {
+        //        priceUsd = value;
+        //        OnPropertyChanged("PriceUsd");
+        //    }
+        //}
+        //public string ChangePercent24H
+        //{
+        //    get { return changePercent24H; }
+        //    set
+        //    {
+        //        changePercent24H = value;
+        //        OnPropertyChanged("changePercent24H");
+        //    }
+        //}
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ TestAssignment/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+
+namespace TestAssignment
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly HttpClient client = new HttpClient();
+        private List<Coin> coins;
+
+        public MainViewModel()
+        {
+            LoadData();
+        }
+
+        public List<Coin> Coins
+        {
+            get => coins;
+            set
+            {
+                coins = value;
+                OnPropertyChanged("Coins");
+            }
+        }
+
+        private async void LoadData()
+        {
+            var response = await client.GetAsync("https://api.coincap.io/v2/assets?limit=10");
+            var coinsJson = await response.Content.ReadAsStringAsync();
+            var coinCapResponse = JsonConvert.DeserializeObject<CoinCapResponse>(coinsJson);
+
+            Coins = result.Data;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
